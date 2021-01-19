cdfe902c0cbeb667097d06984ed367862b7068c2
DoctorWater <75388424+DoctorWater@users.noreply.github.com>
2021-01-19 19:04:17 +0300

    Add files via upload

---
+++ Speak.java
@@ -0,0 +1,7 @@
+package com.company;
+abstract public class Speak {
+    public abstract void curse(String name);
+    public abstract void phrase(String phrase);
+    public abstract void think(String thought);
+    public abstract void tell(String story);
+}
